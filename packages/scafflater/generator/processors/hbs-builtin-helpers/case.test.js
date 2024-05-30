6dd62c73692bcd2dc5c00ee1b1b26dffe2bc7fa9
Emanuel Medeiros <emanuelmbs@protonmail.com>
2024-05-29 21:08:41 -0300

    fix: headerCase and paramCase due to changes in change-case lib

--- packages/scafflater/generator/processors/hbs-builtin-helpers/case.test.js
+++ packages/scafflater/generator/processors/hbs-builtin-helpers/case.test.js
@@ -6,9 +6,9 @@ test("Cases", () => {
   expect(caseHelper("capitalCase", "test string")).toBe("Test String");
   expect(caseHelper("constantCase", "test string")).toBe("TEST_STRING");
   expect(caseHelper("dotCase", "test string")).toBe("test.string");
-  expect(caseHelper("headerCase", "test string")).toBe("Test-String");
+  expect(caseHelper("trainCase", "test string")).toBe("Test-String");
   expect(caseHelper("noCase", "testString")).toBe("test string");
-  expect(caseHelper("paramCase", "test string")).toBe("test-string");
+  expect(caseHelper("kebabCase", "test string")).toBe("test-string");
   expect(caseHelper("noCaseAndTitle", "test-a-string")).toBe("Test a String");
   expect(caseHelper("lowerCase", "Test")).toBe("test");
   expect(caseHelper("upperCase", "Test")).toBe("TEST");
